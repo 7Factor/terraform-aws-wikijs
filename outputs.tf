b2d01872a1f41a07b09ab42532e40be5112361c5
Jeremy Duvall <jduv@7factor.io>
2021-09-28 23:24:08 -0400

    Adding more interesting outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,39 @@
-output "lb_dns" {
-  value       = module.wiki_js.lb_hostname
-  description = "The DNS value of your LB hosting the concourse cluster. Point your FQDN to it."
+output "lb_hostname" {
+  value = module.wiki_js.lb_hostname
+}
+
+output "lb_name" {
+  value = module.wiki_js.lb_name
+}
+
+output "lb_arn" {
+  value = module.wiki_js.lb_arn
+}
+
+output "lb_arn_suffix" {
+  value = module.wiki_js.lb_arn_suffix
+}
+
+output "lb_secure_listener" {
+  value = module.wiki_js.lb_secure_listener
+}
+
+output "lb_target_group_name" {
+  value = module.wiki_js.lb_target_group_name
+}
+
+output "lb_zone_id" {
+  value = module.wiki_js.lb_zone_id
+}
+
+output "health_check_path" {
+  value = module.wiki_js.health_check_path
+}
+
+output "task_definition_arn" {
+  value = module.wiki_js.task_definition_arn
+}
+
+output "service_name" {
+  value = module.wiki_js.service_name
 }
